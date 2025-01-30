2f6d1d510d1a49f1f7c8ce9208ef7d5a703bc7d5
Natapon Pantuwong <pantuwongn@gmail.com>
2025-01-30 10:59:22 -0500

    complete main flow

--- data_processor.py
+++ data_processor.py
@@ -1,4 +1,38 @@
 from datetime import datetime
+from dbHandler import DBHandlerPG, datetime_to_windows_tick, constructDataDict
+import json
+import logging
+import xmltodict
+from logging.handlers import RotatingFileHandler
+from sys import stdout
+
+# Create logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Create file handler and set level to error
+file_handler = RotatingFileHandler(
+    'error.log', maxBytes=1024*1024, backupCount=5)
+file_handler.setLevel(logging.ERROR)
+
+# Create console handler and set level to debug
+console_handler = logging.StreamHandler(stdout)
+console_handler.setLevel(logging.DEBUG)
+
+# Create formatter and add it to the handlers
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+with open('./config.json') as f:
+    config = json.load(f)
+
+dataset_id_to_config = config['FOLDER_CONFIG_DICT']
 
 
 def get_data_by_datetime(datetimes=None):
@@ -8,19 +42,135 @@ def get_data_by_datetime(datetimes=None):
     :param datetimes: None for current time, or list of datetime strings
     :return: Data corresponding to the given datetime(s)
     """
+    dbHandlerPG = DBHandlerPG(
+        url=config['PG_HOST'], username=config['PG_USER'], database=config['PG_NAME'])
+
     if datetimes is None:
         # If no datetime is provided, use the current time
-        dt = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        return f"Data for current datetime: {dt}"
-
-    # Process a list of datetimes
-    results = {}
-    for dt in datetimes:
-        try:
-            # Validate datetime format
+        dt = datetime.now().isoformat()
+        datetimes = [dt]
+    else:
+        for idx in range(len(datetimes)):
             parsed_dt = datetime.strptime(dt, "%Y-%m-%d %H:%M:%S")
-            results[dt] = f"Data for {parsed_dt}"
-        except ValueError:
-            results[dt] = "Invalid datetime format"
+            datetimes[idx] = parsed_dt.isoformat()
 
-    return results
+    # Process for each dataset for each datetime
+    result_dict_list = []
+    for dataset_id in dataset_id_to_config:
+        for dt in datetimes:
+            # if dataset_id is ind01, need to change to windows tick
+            isoDt = dt
+            if dataset_id == 'ind01':
+                dt = datetime_to_windows_tick(dt)
+
+            # query to get a file content of datetime that is previous to dt
+            try:
+                dataset_table = dataset_id.split('___')[0]
+                query_response = dbHandlerPG.query_files_with_dynamic_threshold(
+                    dataset_table, dt)
+            except Exception as e:
+                logger.error(f'{e!r}')
+                break
+
+            if not query_response:
+                # no file content to parse, wait for 10 minutes and try again
+                logger.wanring(f'no record for {
+                               dataset_id} for the datetime as {isoDt}')
+                continue
+
+            record = query_response[0]
+
+            if dataset_id == 'ind01':
+                try:
+                    parsed_dict = xmltodict.parse(record[1])
+                    temp_data_list = parsed_dict['DealingInfo']['Currency']
+                except Exception as e:
+                    logger.error(f'File content cannot be loaded!')
+                    continue
+
+                if not isinstance(temp_data_list, list):
+                    logger.error(
+                        f'File content is not in the expected format')
+                    continue
+
+                for temp_data in temp_data_list:
+                    data_dict = {}
+                    for key, _ in zip(dataset_id_to_config[dataset_id]['keys'], dataset_id_to_config[dataset_id]['data_type']):
+                        if key == 'Symbol':
+                            data_dict[key] = temp_data['@Symbol']
+                        elif key == 'ValueDate':
+                            dt = datetime.datetime.strptime(
+                                temp_data['SSIHistNETME']['ValueDate'], '%Y-%m-%d %H:%M:%S')
+                            data_dict[key] = dt
+                        else:
+                            data_dict[key] = float(
+                                temp_data[key]['Value'])
+                    data_dict['source'] = 'ind01'
+                    data_dict['feed'] = ''
+                    data_dict_list.append(data_dict)
+            else:
+                try:
+                    temp_data = json.loads(record[1])
+                except Exception as e:
+                    logger.error(f'File content cannot be loaded!')
+                    continue
+
+                if not isinstance(temp_data, list):
+                    logger.error(
+                        f'File content is not in the expected format')
+                    continue
+
+                if '___Server' in dataset_id:
+                    id_sp = dataset_id.split('___Server')
+                    data = []
+                    for td in temp_data:
+                        ts = td['time']
+                        serverBBI = td['ServerBBI']
+                        if serverBBI:
+                            for sb in serverBBI:
+                                server_name = sb.pop('Server')
+                                # server_name == f'{id_sp[1]}-Live.mt4tradeserver.com:443' or server_name == f'{id_sp[1]}-live.mt4tradeserver.com:443':
+                                if id_sp[1].lower() in server_name.lower():
+                                    data.append({
+                                        dataset_id_to_config[dataset_id]['timestamp_key']: ts,
+                                        **sb
+                                    })
+                else:
+                    data = temp_data
+
+                for d in data:
+                    if not isinstance(d, dict):
+                        continue
+                    data_dict = constructDataDict(
+                        dataset_id_to_config[dataset_id], d)
+
+                    short = data_dict[dataset_id_to_config[dataset_id]['shortKey']]
+                    long = data_dict[dataset_id_to_config[dataset_id]['longKey']]
+                    bbi = data_dict[dataset_id_to_config[dataset_id]['bbiKey']]
+                    if bbi == 'calculate':
+                        bbi = max(long, short)/min(long, short)
+                        if long < short:
+                            bbi = bbi * -1
+
+                    result_dict = {
+                        'source': None,
+                        'feed': None,
+                        'datetime': isoDt,
+                        'open': None,
+                        'close': None,
+                        'high': None,
+                        'low': None,
+                        'short': short,
+                        'long': long,
+                        'bbi': bbi
+                    }
+                    if '___Server' in dataset_id:
+                        id_sp = dataset_id.split('___Server')
+                        result_dict['source'] = id_sp[0]
+                        result_dict['feed'] = id_sp[1]
+                    else:
+                        result_dict['source'] = dataset_id
+                        result_dict['feed'] = ''
+
+                    result_dict_list.append(result_dict)
+    return result_dict_list
---
+++ dbHandler.py
@@ -0,0 +1,253 @@
+import datetime
+import logging
+import json
+import psycopg2
+import string
+import pytz
+import re
+from logging.handlers import RotatingFileHandler
+from sys import stdout
+from uuid import uuid4
+
+# Create logger
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+# Create file handler and set level to error
+file_handler = RotatingFileHandler(
+    'error.log', maxBytes=1024*1024, backupCount=5)
+file_handler.setLevel(logging.ERROR)
+
+# Create console handler and set level to debug
+console_handler = logging.StreamHandler(stdout)
+console_handler.setLevel(logging.DEBUG)
+
+# Create formatter and add it to the handlers
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(formatter)
+console_handler.setFormatter(formatter)
+
+# Add the handlers to the logger
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+
+with open('./config.json') as f:
+    config = json.load(f)
+
+# dict and list about dataset folder
+dataset_id_to_config = config['FOLDER_CONFIG_DICT']
+dataset_folder_list = list(dataset_id_to_config.keys())
+dataset_to_table_name = {f: f'DATASET_{f}' for f in dataset_folder_list}
+
+# HELPER FUNCTIONS #
+
+
+def parse_timestamp_from_filename(filename):
+    """Parses the timestamp from the filename and converts it to a datetime object."""
+    TIMESTAMP_PATTERN = re.compile(r'(\d{8}-\d{6})')
+    match = TIMESTAMP_PATTERN.search(filename)
+    window_tick_match = re.search(r"_(\d+)\.xml$", filename)
+
+    if match:
+        dt = datetime.datetime.strptime(match.group(1), '%Y%m%d-%H%M%S')
+        # Define Eastern Time (ET) timezone
+        eastern = pytz.timezone('US/Eastern')
+
+        # Localize the datetime object to Eastern Time
+        localized_dt = eastern.localize(dt)
+        return localized_dt
+    elif window_tick_match:
+        windows_tick = int(window_tick_match.group(1))
+        base_date = datetime.datetime(1, 1, 1)
+        dt = base_date + datetime.timedelta(microseconds=windows_tick // 10)
+        # Define Eastern Time (ET) timezone
+        eastern = pytz.timezone('US/Eastern')
+
+        # Localize the datetime object to Eastern Time
+        localized_dt = eastern.localize(dt)
+        return localized_dt
+    else:
+        raise ValueError("Timestamp not found or invalid in filename")
+
+
+def datetime_to_windows_tick(dt, timezone_name='US/Eastern'):
+
+    # Convert to specified timezone (US/Eastern)
+    local_tz = pytz.timezone(timezone_name)
+    dt = dt.replace(tzinfo=pytz.utc).astimezone(local_tz)
+
+    # Define the base date (January 1, Year 1)
+    base_date = datetime.datetime(1, 1, 1, tzinfo=local_tz)
+
+    # Calculate the difference in microseconds
+    delta = dt - base_date
+    microseconds = delta.total_seconds() * 1_000_000
+
+    # Convert to Windows tick (100-nanosecond intervals)
+    windows_tick = int(microseconds * 10)
+
+    return windows_tick
+
+
+def strip_whitespace_and_punctuation(text):
+    # Define a set of whitespace characters and punctuation
+    chars_to_strip = string.whitespace + string.punctuation
+
+    # Strip the characters from the left and right of the string
+    return text.strip(chars_to_strip)
+
+
+def constructParamsString(config, data_dict):
+    data_list = []
+    for key, data_type in zip(config['keys'], config['data_type']):
+        if data_type == 'int':
+            data_dict[key](int(data_dict[key]))
+        elif data_type == 'string':
+            data_list.append(f"'{str(data_dict[key])}'")
+        elif data_type == 'float':
+            data_list.append(float(data_dict[key]))
+        elif data_type == 'bool':
+            data_list.append(data_dict[key])
+    return ', '.join([str(d) for d in data_list])
+
+
+def constructDataDict(config, data_dict):
+    for key, data_type in zip(config['keys'], config['data_type']):
+        if data_dict[key] is None:
+            data_dict[key] = data_dict[key]
+        elif data_type == 'int':
+            data_dict[key] = int(data_dict[key])
+        elif data_type == 'string':
+            data_dict[key] = strip_whitespace_and_punctuation(
+                f"{str(data_dict[key])}")
+        elif data_type == 'float':
+            data_dict[key] = float(data_dict[key])
+        elif data_type == 'bool':
+            data_dict[key] = data_dict[key]
+        elif data_type == 'timestamp':
+            if '.' in data_dict[key]:
+                dt = datetime.datetime.strptime(
+                    data_dict[key], '%Y-%m-%dT%H:%M:%S.%f')
+                data_dict[key] = dt
+            else:
+                dt = datetime.datetime.strptime(
+                    data_dict[key], '%Y-%m-%dT%H:%M:%S')
+                data_dict[key] = dt
+
+    return data_dict
+
+######
+
+
+class DBHandlerPG:
+    def __init__(self, url, username, database):
+        self.url = url
+        self.username = username
+        self.database = database
+
+    def run_query(self, sql_query):
+        try:
+            conn = psycopg2.connect(
+                f"dbname='{self.database}' user='{self.username}' host='{self.url}'")
+        except:
+            logger.error("Cannot connect to the postgresql database")
+            raise
+
+        with conn.cursor() as curs:
+            try:
+                curs.execute(sql_query)
+                results = curs.fetchall()
+                return results
+            # a more robust way of handling errors
+            except (Exception, psycopg2.DatabaseError) as error:
+                logger.error(error)
+                raise
+
+    def query_files_with_dynamic_threshold(self, prefix, threshold=None):
+        try:
+            conn = psycopg2.connect(
+                f"dbname='{self.database}' user='{self.username}' host='{self.url}'")
+        except:
+            logger.error("Cannot connect to the postgresql database")
+            raise
+
+        if '11v' in prefix:
+            suffix = prefix[3:]
+            prefix = '11v'
+
+        # Determine if the threshold is a tick or a timestamp
+        is_tick = isinstance(threshold, int)
+
+        if is_tick:
+            query = r"""
+                    WITH extracted AS (
+                        SELECT
+                            file_path,
+                            file_content,
+                            regexp_match(file_path, '^.*[\\/](\w+)_(\d+)\.xml$') AS match_result
+                        FROM
+                            files
+                        WHERE file_path LIKE %s
+                    )
+                    SELECT
+                        file_path,
+                        file_content,
+                        match_result[1] AS prefix,
+                        match_result[2] AS windows_tick
+                    FROM
+                        extracted
+                    WHERE
+                        match_result IS NOT NULL AND
+                        match_result[1] ~ ('^' || %s || '(_|$)')
+                    """
+            params = [f"%{prefix}%", prefix]
+            if threshold is not None:
+                query += " AND CAST(match_result[2] AS BIGINT) > %s"
+                params.append(threshold)
+
+            # Add LIMIT clause
+            query += " ORDER BY match_result[2] LIMIT 100;"
+        else:
+            query = r"""
+                    WITH extracted AS (
+                        SELECT
+                            file_path,
+                            file_content,
+                            regexp_match(file_path, '^.*[\\/](\w+)_.*_(\d{8}-\d{6})\.json$') AS match_result
+                        FROM
+                            files
+                        WHERE file_path LIKE %s
+                    )
+                    SELECT
+                        file_path,
+                        file_content,
+                        match_result[1] AS prefix,
+                        match_result[2] AS timestamp
+                    FROM
+                        extracted
+                    WHERE
+                        match_result IS NOT NULL AND
+                        match_result[1] ~ ('^' || %s || '(_|$)')
+                    """
+            params = [f"%{prefix}%", prefix]
+            if threshold is not None:
+                query += " AND to_timestamp(match_result[2], 'YYYYMMDD-HH24MISS') > %s::timestamp"
+                params.append(threshold)
+
+            # Add LIMIT clause
+            query += " ORDER BY match_result[2] LIMIT 100;"
+
+        with conn.cursor() as cur:
+            cur.execute(query, params)
+            results = cur.fetchall()
+            if prefix not in ['11v', '11vab']:
+                return results
+            else:
+                filtered_results = []
+                for r in results:
+                    if prefix == '11v' and 'vab' not in r[0] and f'_{suffix}_' in r[0]:
+                        filtered_results.append(r)
+                    elif prefix == '11vab' and 'vab' in r[0] and f'_{suffix}_' in r[0]:
+                        filtered_results.append(r)
+                return filtered_results
